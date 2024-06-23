c9c81c7dc6e56b9ede9f65f82be74cf4cf9c68c6
mim chowdhury <umac3333@gmail.com>
2024-06-23 10:00:52 +0600

    submit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,18 @@ import Banner from '../Banner/Banner';
 import Tourism from '../Tourism/Tourism';
 import TourType from '../TourType/TourType';
 import Story from '../Story/Story';
-
+import { motion, useScroll, useSpring } from "framer-motion";
+import './Motion.css'
 const Home = () => {
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    });
     return (
         <div>
+            <motion.div className="progress-bar my-14 z-40" style={{ scaleX }} />
             <Banner></Banner>
             <Tourism></Tourism>
             <TourType></TourType>
